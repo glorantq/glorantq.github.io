5f2ad3abd9e4e4e5d90f58872fea907ba1f511dd
Gerber Lóránt Viktor <glorantq@gmail.com>
2017-09-08 23:15:17 +0200

    Particles

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,20 +1,19 @@
 particlesJS("particles-js", { "particles": { "number": { "value": 95, "density": {
-"enable": true, "value_area": 552.4033491425909 } }, "color": {
-"value":"#ffffff" }, "shape": { "type": "circle", "stroke": { "width": 0,
-"color":"#000000" }, "polygon": { "nb_sides": 4 }, "image": { "src":
-"img/github.svg", "width": 100, "height": 100 } }, "opacity": { "value":
-0.4008530152163807, "random": false, "anim": { "enable": false, "speed": 1,
-"opacity_min": 0.1, "sync": false } }, "size": { "value": 1, "random": true,
-"anim": { "enable": true, "speed": 4.872463273808071, "size_min":
-2.436231636904035, "sync": true } }, "line_linked": { "enable": true,
-"distance": 144.30708547789706, "color": "#ffffff", "opacity":
-0.5371430403899501, "width": 0.8017060304327615 }, "move": { "enable": true,
-"speed": 6, "direction": "none", "random": true, "straight": false, "out_mode":
-"out", "bounce": false, "attract": { "enable": true, "rotateX": 600, "rotateY":
-1200 } } }, "interactivity": { "detect_on": "canvas", "events": { "onhover": {
-"enable": false, "mode": "repulse" }, "onclick": { "enable": true, "mode":
-"push" }, "resize": true }, "modes": { "grab": { "distance": 487.24632738080703,
-"line_linked": { "opacity": 1 } }, "bubble": { "distance": 400, "size": 40,
-"duration": 2, "opacity": 8, "speed": 3 }, "repulse": { "distance":
-73.08694910712106, "duration": 0.4 }, "push": { "particles_nb": 4 }, "remove": {
-"particles_nb": 2 } } }, "retina_detect": true } );
+"enable": true, "value_area": 552.4033491425909 } }, "color": { "value":
+"#ffffff" }, "shape": { "type": "circle", "stroke": { "width": 0, "color":
+"#000000" }, "polygon": { "nb_sides": 4 }, "image": { "src": "img/github.svg",
+"width": 100, "height": 100 } }, "opacity": { "value": 0.4008530152163807,
+"random": false, "anim": { "enable": false, "speed": 1, "opacity_min": 0.1,
+"sync": false } }, "size": { "value": 1, "random": true, "anim": { "enable":
+true, "speed": 4.872463273808071, "size_min": 2.436231636904035, "sync": true } },
+"line_linked": { "enable": true, "distance": 144.30708547789706, "color":
+"#ffffff", "opacity": 0.5371430403899501, "width": 0.8017060304327615 }, "move": {
+"enable": true, "speed": 6, "direction": "none", "random": true, "straight":
+false, "out_mode": "out", "bounce": false, "attract": { "enable": true,
+"rotateX": 600, "rotateY": 1200 } } }, "interactivity": { "detect_on": "canvas",
+"events": { "onhover": { "enable": false, "mode": "repulse" }, "onclick": {
+"enable": true, "mode": "push" }, "resize": true }, "modes": { "grab": {
+"distance": 487.24632738080703, "line_linked": { "opacity": 1 } }, "bubble": {
+"distance": 400, "size": 40, "duration": 2, "opacity": 8, "speed": 3 },
+"repulse": { "distance": 73.08694910712106, "duration": 0.4 }, "push": {
+"particles_nb": 4 }, "remove": { "particles_nb": 2 } } }, "retina_detect": true });
